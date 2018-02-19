4c6f5059989a565adfec4649ce90816374c1aa68
Henry Singleton <henry.singleton@squareweave.com.au>
2018-02-19 20:08:50 +1100

    Remove unused template debug thing

--- graniteoctopus/settings/dev.py
+++ graniteoctopus/settings/dev.py
@@ -3,7 +3,6 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '*p15w7sp-72w@=^09zgib8i9mw=2n_&n@=d(_vkp%dan(&_noz'
